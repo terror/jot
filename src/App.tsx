d37c864554cfc50c192935bdf4e7cff1bd4afcfe
Liam <liam@scalzulli.com>
2025-04-08 16:20:34 -0400

    Add italic extension

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Bold from '@tiptap/extension-bold';
 import Document from '@tiptap/extension-document';
 import Heading from '@tiptap/extension-heading';
 import HorizontalRule from '@tiptap/extension-horizontal-rule';
+import Italic from '@tiptap/extension-italic';
 import Paragraph from '@tiptap/extension-paragraph';
 import Text from '@tiptap/extension-text';
 import { EditorContent, useEditor } from '@tiptap/react';
@@ -24,6 +25,7 @@ function App() {
       CustomHorizontalRule,
       Document,
       Heading.configure({ levels: [1, 2, 3] }),
+      Italic,
       Paragraph,
       Text,
     ],
