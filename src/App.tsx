3ea087770535ffeceb73c0522a6a6d024bccd661
Liam <liam@scalzulli.com>
2025-04-08 11:48:20 -0400

    Handle state for textarea value

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
+import { useState } from "react";
+
 function App() {
+  const [value, setValue] = useState<string>('');
+
   return (
     <div className='flex h-screen w-screen items-center justify-center bg-gray-100'>
       <textarea
         autoComplete='off'
         autoCorrect='off'
         className='h-full w-full resize-none p-4 focus:outline-none'
+        onChange={(e) => setValue(e.target.value)}
         spellCheck={false}
+        value={value}
       />
     </div>
   );
